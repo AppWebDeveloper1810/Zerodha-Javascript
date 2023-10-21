a511053661ab48b2cf8cf46f853239d964bedde5
AppWebDeveloper1810 <stock2k0@gmail.com>
2023-10-21 19:16:38 +0530

    Add module export for User class in user.js

--- user.js
+++ user.js
@@ -71,3 +71,5 @@ const userInstance = new User();
 console.log(userInstance.registerUser("alice", "alice@example.com", "secret_password"));
 console.log(userInstance.login("alice", "wrong_password"));
 console.log(userInstance.login("bob", "some_password"));
+
+module.exports = User;
