c3c496c1ece611dcfd488c43166d21007354ad8a
AppWebDeveloper1810 <stock2k0@gmail.com>
2023-10-22 15:33:55 +0530

    Add getStockSummary in miscellaneous.js

--- miscellaneous.js
+++ miscellaneous.js
@@ -1,27 +1,39 @@
 const axios = require('axios');
+const secrets = require('./secrets');
 
-const apiKey = require('./secrets'); // Get your API key from Alpha Vantage
+async function getStockSummary(symbol, region) {
+  const options = {
+    method: 'GET',
+    url: 'https://zerodha-javascript.p.rapidapi.com/stock/v2/get-summary',
+    params: {
+      symbol,
+      region,
+    },
+    headers: {
+      'X-RapidAPI-Key': secrets[1],
+      'X-RapidAPI-Host': secrets[0],
+    },
+  };
 
-async function getStockInfo(stockSymbol) {
   try {
-    const response = await axios.get(
-      `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stockSymbol}&apikey=${apiKey}`
-    );
-
-    if (response.data) {
-      const data = response.data;
-      const country = data.Country;
-      const currency = data.Currency;
-      console.log(`Stock Symbol: ${stockSymbol}`);
-      console.log(`Country: ${country}`);
-      console.log(`Currency: ${currency}`);
-    } else {
-      console.log('Stock information not found.');
-    }
+    const response = await axios.request(options);
+    return response.data;
   } catch (error) {
-    console.error('Error:', error.message);
+    throw error;
   }
 }
 
-const stockSymbol = 'AAPL'; // Replace with the desired stock symbol
-getStockInfo(stockSymbol);
+// Example usage:
+async function main() {
+  try {
+    const symbol = 'AMRN'; // Replace with the desired stock symbol
+    const stockSummary = await getStockSummary(symbol);
+    const data = stockSummary['summaryDetail'];
+    console.log(data.currency);
+    console.log(stockSummary['quoteType'].market.split('_')[0]);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = getStockSummary;
