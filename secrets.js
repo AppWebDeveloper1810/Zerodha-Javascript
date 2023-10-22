4ac7c5d1edcee7b0c8e1c25c7374ded735722fd9
AppWebDeveloper1810 <stock2k0@gmail.com>
2023-10-22 15:36:10 +0530

    Add secrets.js file

--- secrets.js
+++ secrets.js
@@ -1,2 +1,4 @@
 const ALPHA_VANTAGE_API_KEY = "6X3CIWDEVGUO73K2";
-module.exports = ALPHA_VANTAGE_API_KEY
+X_RapidAPI_Key = '611fec5040msh35e4ed03a346ef0p16e224jsn8e262de234f4';
+X_RapidAPI_Host = 'apidojo-yahoo-finance-v1.p.rapidapi.com';
+module.exports = [X_RapidAPI_Host, X_RapidAPI_Key]
